6b2ed842ac2949cfb64ba306622ee7fd275c9e04
Robert <rbasi008@uottawa.ca>
2021-07-15 01:11:00 -0400

    plsdfgdsg

--- ohmysweetreats/src/App.js
+++ ohmysweetreats/src/App.js
@@ -1,21 +1,37 @@
- 
+
 import "./App.css";
 import Navigation from "./Navigation";
 import Home from "./Home";
 import Variation from "./Variation";
-import Options  from "./Options";
+import Options from "./Options";
 import Footer from "./Footer";
+
+import Quote from './Quote';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-       <Navigation></Navigation>
-       <Home></Home>
-       <Variation></Variation> 
-       <Options></Options>
-       <Footer></Footer>
+    <Router>
+      <div>
+ 
+
+          <Navigation/>
+          <Switch>
+            <Route  exact path="/">
+              <Home></Home>
+              <Variation></Variation>
+              <Options></Options>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/quote">
+              <Quote></Quote>
+            </Route>
+          </Switch>
     </div>
-  );
+    </Router>
+
+      );
 }
 
-export default App;
+      export default App;
--- ohmysweetreats/src/Navigation.js
+++ ohmysweetreats/src/Navigation.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import Quote from './other/Quote';
+import Quote from './Quote';
+import { Link } from "react-router-dom";
 
 
 class Navigation extends Component {
@@ -23,10 +24,10 @@ class Navigation extends Component {
                                 <a href="#" className="brand-logo"><h1>OhMySweeTreats</h1></a>
                                 <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href={Quote}>Request a quote</a></li>
-                                    <li><a href="#">Our Menu</a></li>
-                                    <li><a href="#">FAQ</a></li>
+                                    <li><Link to="/" >Home</Link></li>
+                                    <li><Link to="/quote">Request a quote</Link></li>
+                                    <li><Link to="/" >Our Menu</Link></li>
+                                    <li><Link to="/" >FAQ</Link></li>
                                 </ul>
                             </div>
                         </nav>
